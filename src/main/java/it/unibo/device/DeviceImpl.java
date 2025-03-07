babfac8261f94597c02ccbce02f8adfe63e43b8b
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-07 11:35:15 +0100

    feat: add LLM implementation for Device interface

--- src/main/java/it/unibo/device/DeviceImpl.java
+++ src/main/java/it/unibo/device/DeviceImpl.java
@@ -1,2 +1,27 @@
-package it.unibo.device;public class DeviceImpl {
+package it.unibo.device;
+
+public class DeviceImpl implements Device {
+    private boolean isOn;
+
+    public void on() throws IllegalStateException {
+        if (isOn) {
+            throw new IllegalStateException();
+        }
+        isOn = true;
+    }
+
+    public void off() {
+        if (isOn) {
+            throw new IllegalStateException();
+        }
+        isOn = false;
+    }
+
+    public boolean isOn() throws IllegalStateException {
+        return isOn;
+    }
+
+    public void reset() throws IllegalStateException {
+        isOn = false;
+    }
 }
